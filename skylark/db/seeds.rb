e26167d045b98ba30f992b6607330f726f1c06ba
Michael <email": null>
2016-02-19 16:50:12 -0800

    develop user profile view

--- skylark/db/seeds.rb
+++ skylark/db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+user = User.new(
+  username: 'Steve',
+  email: 'steve@email.com',
+  password: '12345678')
+user.save
+
+user2 = User.new(
+  username: 'Amy',
+  email: 'amy@email.com',
+  password: '12345678')
+user2.save
+
+5.times do
+  date = "2016-" + rand(1..12).to_s + "-" + rand(1..28).to_s
+  Image.create(
+    user_id: 1,
+    title: Faker::Hacker.adjective + Faker::Hacker.noun,
+    tileset: "whoosawhatsits2245624gfg",
+    date_taken: date,
+    description: Faker::Lorem.paragraph,
+    camera_type: Faker::Hacker.abbreviation
+    )
+end
+
+5.times do
+  date = "2016-" + rand(1..12).to_s + "-" + rand(1..28).to_s
+  Image.create(
+    user_id: 2,
+    title: Faker::Hacker.adjective + Faker::Hacker.noun,
+    tileset: "whargabrlefrabblebab54624",
+    date_taken: date,
+    description: Faker::Lorem.paragraph,
+    camera_type: Faker::Hacker.abbreviation
+    )
+end
