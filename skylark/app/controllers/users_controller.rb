3ec74f0d9e5701fe6de360c8d063cf887901b0a2
Michael <email": null>
2016-02-20 20:24:20 -0800

    Implement simple auth check on User profile page

--- skylark/app/controllers/users_controller.rb
+++ skylark/app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
+# slightly janky - redirects any requests to users/:id to own profile if logged in
+# a better version: show limited info on a particular user to others (list of images posted)
   def show
-    @user = User.find(params[:id])
+    if user_signed_in?
+      @user = User.find(current_user.id)
+    else
+      redirect_to new_user_session_path
+    end
   end
 
 end
