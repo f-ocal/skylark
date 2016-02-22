1179b6e6fd98a6cbd162983afbe82ef50a13bfd8
Michael <email": null>
2016-02-22 15:46:43 -0800

    Implement first basic Model tests

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
-# RSpec.describe Image, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Image, type: :model do
+  let(:image_no_date) { Image.new(
+      user_id: 1,
+      tileset_name: "Tile Set Name",
+      map: "jarjar.8binks",
+      description: "What a country!",
+      camera_type: "BDSMSLR"
+    )}
 
-# Jordan lecagy code, re-adapted
-describe Image do
-  context 'validations' do
-    it { should validate_presence_of :tileset_name }
-    it { should validate_presence_of :date_taken }
-    it { should validate_presence_of :description}
-    it { should validate_presence_of :camera_type }
+  describe 'validations' do
+    context 'will raise an error' do
+      it 'when the date field is empty' do
+        image_no_date.save
+        expect(image_no_date.errors[:date_taken]).to include("can't be blank")
+      end
+    end
   end
 end
+
+# Jordan lecagy code, re-adapted
+# describe Image do
+#   context 'validations' do
+#     it { should validate_presence_of :tileset_name }
+#     it { should validate_presence_of :date_taken }
+#     it { should validate_presence_of :description}
+#     it { should validate_presence_of :camera_type }
+#   end
+# end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_no_name) { User.new(
+    email: 'steve@email.com',
+    password: '12345678'
+    )}
+
+  describe 'validations' do
+    context 'will raise an error' do
+      it 'when the username field is empty' do
+        user_no_name.save
+        expect(user_no_name.errors[:username]).to include("can't be blank")
+      end
+    end
+  end
 end
 
 # Jordan lecagy code
