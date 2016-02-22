6ebce3f001b1818912afa871111d3435af4eb60c
Karla <karlaking2@gmail.com>
2016-02-21 21:59:14 -0800

    Include images-new

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -69,9 +69,9 @@ class ImagesController < ApplicationController
 
     def create_image_in_mapbox(tileset_name)
       HTTParty.post('https://api.mapbox.com/uploads/v1/f-ocal?access_token=sk.eyJ1IjoiZi1vY2FsIiwiYSI6ImNpa3ZneGFpYzAwZnV1bWtzczA2YWQ5OTQifQ.Eqezri-fTOcuCfv_mMTCuw',
-        :body => {  "tileset": "f-ocal.#{@key[12..36]}",
-                    "url": @url,
-                    "name": tileset_name
+        :body => {  "tileset" => "f-ocal.#{@key[12..36]}",
+                    "url"=> @url,
+                    "name" => tileset_name
                   }.to_json,
         :headers => { 'Content-Type' => 'application/json'}
       )
