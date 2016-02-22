c0f3b37467417d639d459c73dd3fb4e4e7d9265e
Michael <email": null>
2016-02-22 15:22:14 -0800

    Save before pulling

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-# Jordan lecagy code
-# describe Article do
-#   context 'validations' do
-#     it { should validate_presence_of :title }
-#     it { should validate_presence_of :body }
-#   end
+# RSpec.describe Image, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
 # end
+
+# Jordan lecagy code, re-adapted
+describe Image do
+  context 'validations' do
+    it { should validate_presence_of :tileset_name }
+    it { should validate_presence_of :date_taken }
+    it { should validate_presence_of :description}
+    it { should validate_presence_of :camera_type }
+  end
+end
