5714870166584f70cae82890d49cd931d28fbc92
Michael <email": null>
2016-02-23 10:43:50 -0800

    Refactor Users controller and profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
 
-# slightly janky - redirects any requests to users/:id to own profile if logged in
+  before_action :authenticate_user!, only: [:show]
+# redirects any requests to users/:id to own profile if logged in
 # a better version: show limited info on a particular user to others (list of images posted)
   def show
-    if user_signed_in?
-      @user = User.find(current_user.id)
-      @images = Image.where(user_id: current_user.id)
-    else
-      redirect_to new_user_session_path
-    end
+    @user = User.find(current_user.id)
   end
 
 end
