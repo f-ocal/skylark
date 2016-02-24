ecc2b128116733aa1dcb526414b3521b032c6d4a
Natasha Thapliyal <email": null>
2016-02-24 12:42:01 -0800

    Add opacity to all images

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -4,17 +4,31 @@ $(document).ready(function(){
 
 //put access key in ENV variable
 L.mapbox.accessToken = 'pk.eyJ1Ijoic2hhd25zcGVhcnMiLCJhIjoiY2lrdDJ2ZHI1MDAyNHZvbTNhZ3B2bm41MCJ9.WdbcbGGIoMKx9B3sC9ODuQ';
-
-
 var mapGeo = L.mapbox.map('map_geo', 'mapbox.satellite').setView([27.433,-1.700], 3);
 
-  $('.sidebar').hide();
+$('.sidebar').hide();
 
+
+
+  //Turn gon image into layer
+  var layers = []
   gon.images.forEach ( function (image) {
-    console.log(image);
-    var layer = L.mapbox.tileLayer(image.map, {
-      format: 'png128'
-    });
+    var layer = L.mapbox.tileLayer(image.map, {format: 'png128' });
+    addMarkerstoLayer(mapGeo, layer, image);
+    layers.push(layer)
+    return layers;
+  });
+
+    //Opacity handle
+    opacityHandle(layers);
+
+
+});
+
+
+
+var addMarkerstoLayer = function(map, layer, image){
+    //add markers to layer
     layer.on('ready', function() {
       var tileJSON = layer.getTileJSON();
 
@@ -33,80 +47,80 @@ var mapGeo = L.mapbox.map('map_geo', 'mapbox.satellite').setView([27.433,-1.700]
           'marker-symbol': 'marker-stroked',
         })
       });
+      //add layer and marker to map
+      layer.addTo(map)
+      marker.addTo(map)
 
-      layer.addTo(mapGeo)
-      marker.addTo(mapGeo)
+      displayImageInfoOnClick(map, marker, image)
+    });
+  }
 
-      //Opacity handle
-      opacityHandle(layer);
-
-    //Marker setup
-      marker.on('click', function(e){
-        mapGeo.setView([e.latlng.lat, e.latlng.lng], 14)
-
-         //Toggle Sidebar
-        $('.sidebar').show();
-        $('#close-sb').on('click', function(){
-          $('.sidebar').hide();
-        })
-
+  var displayImageInfoOnClick = function(map, marker, image){
+    marker.on('click', function(e){
+      map.setView([e.latlng.lat, e.latlng.lng], 14)
 
        //Empty and Fill Info div with image information
-        var tilesetName = image.tileset_name
-        var tlNameH2Tag = '<h2 class="sb-tl-name">' + tilesetName + '</h2>'
+       var tilesetName = image.tileset_name
+       var tlNameH2Tag = '<h2 class="sb-tl-name">' + tilesetName + '</h2>';
+       var username = image.username;
+       var usernamePTag = '<p class="sb-username"> Mapmaker: ' + image.username + '</p>';
+       var cameraType = image.camera_type;
+       var cameraTypePTag = '<p class="sb-camera-type"> Camera Type: ' + cameraType + '</p>';
+       var descr = image.description;
+       var descrPTag = '<p class="sb-desc">' + descr + '</p>';
 
-        var username = image.username
-        var usernamePTag = '<p class="sb-username"> Mapmaker: ' + image.username + '</p>'
+       $('#info').empty();
+       $('#info').append(tlNameH2Tag + usernamePTag + cameraTypePTag + descrPTag);
 
-        var cameraType = image.camera_type
-        var cameraTypePTag = '<p class="sb-camera-type"> Camera Type: ' + cameraType + '</p>'
-
-        var descr = image.description
-        var descrPTag = '<p class="sb-desc">' + descr + '</p>'
-
-        $('#info').empty();
-        $('#info').append(tlNameH2Tag + usernamePTag + cameraTypePTag + descrPTag)
+         //Toggle Sidebar
+         $('.sidebar').show();
+         $('#close-sb').on('click', function(){
+          $('.sidebar').hide();
         });
 
          //Add and Remove marker on zoom
-          mapGeo.on('zoomend', function(){
-            if(mapGeo.getZoom() >= 10){
-              marker.setOpacity(0);
-            } else {
-              marker.setOpacity(1);
-            }
-          });
+         map.on('zoomend', function(){
+          if(map.getZoom() >= 10){
+            marker.setOpacity(0);
+          } else {
+            marker.setOpacity(1);
+          }
         });
-      });
 
-  });
+       });
+    }
 
-  var opacityHandle = function(image_layer){
-    var handle = document.getElementById('handle'),
-    start = false,
-    startTop;
+var opacityHandle = function(image_layers){
+  var handle = document.getElementById('handle'),
+  start = false,
+  startTop;
 
-    document.onmousemove = function(e){
-      if(!start) return;
+  document.onmousemove = function(e){
+    if(!start) return;
       //Adjust Control
       handle.style.top = Math.max(-5, Math.min(195, startTop + parseInt(e.clientY, 10) - start)) + 'px';
       //Adjust opacity -- loop through images
-      image_layer.setOpacity(1 - (handle.offsetTop / 200));
+      for(var i=0; i < image_layers.length; i++){
+        image_layers[i].setOpacity(1 - (handle.offsetTop / 200));
+      }
     }
 
     handle.onmousedown = function(e) {
       // Record initial positions.
-      console.log("working??")
       start = parseInt(e.clientY, 10);
       startTop = handle.offsetTop - 5;
       return false;
     };
 
     document.onmouseup = function(e) {
-        start = null;
+      start = null;
     };
-
   }
+
+
+
+
+
 
 // Images Index
   //loads all markers, global view - DONE
