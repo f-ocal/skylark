e461a701280b29b2bbbc0ef66ff6e95051946912
Michael <email": null>
2016-02-23 17:20:59 -0800

    Update rails-helper file

--- spec/features/images_spec.rb
+++ spec/features/images_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "Images", type: :feature do
   context 'a guest user' do
     it 'can see the page title' do
       visit root_path
+      expect(page).to have_content('SKYLARK')
     end
   end
 
